d0e4e31d7bc34fa73f1c1ebc581ea470be940886
Anupam Singh <132549439+anupamsingh88@users.noreply.github.com>
2025-06-04 23:04:33 +0530

    Inserted Elements using js and using grid concept

--- DOM/Ques1/script.js
+++ DOM/Ques1/script.js
@@ -1,8 +1,16 @@
 let divs= document.querySelectorAll("div");
+let Body = document.querySelector("body")
 
-let ind =1;
 
+ let ind = 1
 divs.forEach(val => {
     val.innerText=`This is div ${ind}`;
     ind++;
 })
+
+let newdiv = document.createElement("div")
+newdiv.innerText = "This is div";
+newdiv.className = "box"
+newdiv.style.backgroundColor = "aliceblue"
+
+Body.prepend(newdiv)
