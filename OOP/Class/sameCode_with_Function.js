36d73748bc0f7db40e0d24351b69a40ab915b627
Anupam Singh <132549439+anupamsingh88@users.noreply.github.com>
2025-11-07 16:31:25 +0530

    Fix condition checks in CheckNumSeats function for accurate seat count

--- OOP/Class/sameCode_with_Function.js
+++ OOP/Class/sameCode_with_Function.js
@@ -16,16 +16,15 @@ function startEngine(Car){
 
 // Method to check the number of seats
 function CheckNumSeats(Car){
-  if('type'=== 'Sedan'){
+  if(Car.type === 'Sedan'){
     return `Number of seats of the ${Car.brand} is 4`
   }
-  else if('type' === 'Sports'){
+  else if(Car.type === 'Sports'){
     return `Number of seats in the ${Car.brand} is 2`
   }
-  else if('type'==='SUV'){
+  else if(Car.type ==='SUV'){
     return `Number of seats of the ${Car.brand} is 6`
   }
-
   else{
     return `Enter a relevant type of the car`;
   }
@@ -33,3 +32,10 @@ function CheckNumSeats(Car){
 
 let Car1 = Car('BMW','M5','Black','Sports');
 startEngine(Car1);
+displayDetails(Car1);
+console.log(CheckNumSeats(Car1));
+console.log('--------------------------');
+let Car2 = Car('Audi','A4','White','Sedan');
+startEngine(Car2);
+displayDetails(Car2);
+console.log(CheckNumSeats(Car2));
